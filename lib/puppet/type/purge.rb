641e1a145b93b1698a47fa54c5eb93aa208901f6
Craig Dunn <craig@craigdunn.org>
2016-04-29 09:15:39 +0200

    updates

--- lib/puppet/type/purge.rb
+++ lib/puppet/type/purge.rb
@@ -11,10 +11,45 @@ Puppet::Type.newtype(:purge) do
     end
   end
 
-  newparam(:if, :array_matching => :all) do
-  end
 
-  newparam(:unless, :array_matching => :all) do
+  [ :unless, :if ]. each do |param_name|
+    newparam(param_name, :array_matching => :all) do
+
+      desc "Purge these resources #{param_name.to_s} any of the criterias match"
+
+      validate do |cond|
+        raise ArgumentError, "must be an array" unless cond.is_a?(Array)
+      end
+  
+      munge do |cond|
+        if cond[0].is_a?(Array)
+          cond
+        else
+          [ cond ]
+        end
+      end
+  
+      validate do |cond_param|
+        case cond_param[0]
+        when String
+          cond = [ cond_param ]
+        when Array
+          cond = cond_param
+        end
+  
+        cond.each do |param, operator, value|
+          unless ["!=","==","=~",">","<","<=","=>"].include?(operator)
+            raise ArgumentError, "invalid operator #{operator}"
+          end
+  
+          unless param && operator && value
+            raise ArgumentError, "not enough parameters given for filter"
+          end
+        end
+      end
+    end
+
+
 
   end
 
@@ -35,6 +70,7 @@ Puppet::Type.newtype(:purge) do
     purged_resources
   end
 
+  # purge will evaluate the conditions given in if/unless
   def purge?(res_type)
 
     res = res_type.to_resource.to_hash
@@ -50,8 +86,11 @@ Puppet::Type.newtype(:purge) do
     return true
   end
 
+  # evaluate_resources loops through the array of param, operator, value
+  # and returns true if any of the criteria match
+  #
   def evaluate_resource(res,condition)
-    condition.select  {  |operator, param, value|
+    condition.select  {  |param, operator, value|
       case operator
       when "!=", "=="
         res[param.to_sym].to_s.method(operator).(value)
